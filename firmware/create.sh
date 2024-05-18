81765fcf3874cf8483ab56f1995fd311993f4db4
Jason Pell <jason@pellcorp.com>
2024-05-18 12:30:35 +1000

    prefix version with 5. and rewrite the ota_info file

--- firmware/create.sh
+++ firmware/create.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# if you look hard enough you can find the password on the interwebs in a certain discord
+password="$(cat ~/.k1/firmware.passwd)"
+if [ -z "$password" ]; then
+    echo "Creality K1 firmware password missing - should be in ~/.k1/firmware.passwd!!!"
+    exit 1
+fi
+
 commands="7z unsquashfs mksquashfs"
 for command in $commands; do
     command -v "$command" > /dev/null
@@ -9,46 +16,53 @@ for command in $commands; do
     fi
 done
 
+old_version=1.3.3.8
+version="5.${old_version}"
+
+function write_ota_info() {
+    echo "ota_version=${version}" > /tmp/${version}-pellcorp/ota_info
+    echo "ota_board_name=${board_name}" >> /tmp/${version}-pellcorp/ota_info
+    echo "ota_compile_time=$(date '+%Y %m.%d %H:%M:%S')" >> /tmp/${version}-pellcorp/ota_info
+    echo "ota_site=http://192.168.43.52/ota/board_test" >> /tmp/${version}-pellcorp/ota_info
+    sudo cp /tmp/${version}-pellcorp/ota_info /tmp/${version}-pellcorp/squashfs-root/etc/
+}
+
 function update_rootfs() {
     pushd /tmp/${version}-pellcorp/ > /dev/null
     sudo unsquashfs orig_rootfs.squashfs 
-    #echo "I was here" > /tmp/${version}-pellcorp/squashfs-root/etc/me
+    write_ota_info
     sudo mksquashfs squashfs-root rootfs.squashfs || exit $?
     sudo rm -rf squashfs-root
     sudo chown $USER rootfs.squashfs 
 }
 
-# if you look hard enough you can find the password on the interwebs in a certain discord
-password="$(cat ~/.k1/firmware.passwd)"
-version=1.3.3.8
-
-if [ -z "$password" ]; then
-    echo "Creality K1 firmware password missing - should be in ~/.k1/firmware.passwd!!!"
-    exit 1
-fi
-
-download=$(wget -q https://www.creality.com/pages/download-k1-flagship -O- | grep -o  "\"\(.*\)V${version}.img\"" | head -1 | tr -d '"')
-filename=$(basename $download)
-directory=$(echo $filename | sed 's/\.img//g')
+download=$(wget -q https://www.creality.com/pages/download-k1-flagship -O- | grep -o  "\"\(.*\)V${old_version}.img\"" | head -1 | tr -d '"')
+old_image_name=$(basename $download)
+# CR4CU220812S11_ota_img_V1.3.3.8
+board_name=$(echo "$old_image_name" | grep -oh "CR[^_\]*")
+old_directory="${board_name}_ota_img_V${old_version}"
+old_sub_directory="ota_v${old_version}"
+directory="${board_name}_ota_img_V${version}"
 sub_directory="ota_v${version}"
+image_name="${board_name}_ota_img_V${version}".img
 
-if [ ! -f /tmp/$filename ]; then
-    echo "Downloading $download -> /tmp/$filename ..."
-    wget "$download" -O /tmp/$filename
+if [ ! -f /tmp/$old_image_name ]; then
+    echo "Downloading $download -> /tmp/$old_image_name ..."
+    wget "$download" -O /tmp/$old_image_name
 fi
 
-if [ -d /tmp/$directory ]; then
-    rm -rf /tmp/$directory
+if [ -d /tmp/$old_directory ]; then
+    rm -rf /tmp/$old_directory
 fi
 
-7z x /tmp/$filename -p"$password" -o/tmp
+7z x /tmp/$old_image_name -p"$password" -o/tmp
 
 if [ -d /tmp/${version}-pellcorp ]; then
     sudo rm -rf /tmp/${version}-pellcorp
 fi
 mkdir -p /tmp/${version}-pellcorp/$directory/$sub_directory
 
-cat /tmp/$directory/$sub_directory/rootfs.squashfs.* > /tmp/${version}-pellcorp/orig_rootfs.squashfs
+cat /tmp/$old_directory/$old_sub_directory/rootfs.squashfs.* > /tmp/${version}-pellcorp/orig_rootfs.squashfs
 orig_rootfs_md5=$(md5sum /tmp/${version}-pellcorp/orig_rootfs.squashfs | awk '{print $1}')
 orig_rootfs_size=$(stat -c%s /tmp/${version}-pellcorp/orig_rootfs.squashfs)
 
@@ -61,11 +75,11 @@ rootfs_size=$(stat -c%s  /tmp/${version}-pellcorp/rootfs.squashfs)
 echo "current_version=$version" > /tmp/${version}-pellcorp/$directory/ota_config.in
 echo "" > /tmp/${version}-pellcorp/$directory/$sub_directory/ota_v${version}.ok
 
-cp /tmp/$directory/$sub_directory/ota_update.in /tmp/${version}-pellcorp/$directory/$sub_directory/
-cp /tmp/$directory/$sub_directory/ota_md5_xImage* /tmp/${version}-pellcorp/$directory/$sub_directory/
-cp /tmp/$directory/$sub_directory/ota_md5_zero.bin* /tmp/${version}-pellcorp/$directory/$sub_directory/
-cp /tmp/$directory/$sub_directory/zero.bin.* /tmp/${version}-pellcorp/$directory/$sub_directory/
-cp /tmp/$directory/$sub_directory/xImage.* /tmp/${version}-pellcorp/$directory/$sub_directory/
+cp /tmp/$old_directory/$old_sub_directory/ota_update.in /tmp/${version}-pellcorp/$directory/$sub_directory/
+cp /tmp/$old_directory/$old_sub_directory/ota_md5_xImage* /tmp/${version}-pellcorp/$directory/$sub_directory/
+cp /tmp/$old_directory/$old_sub_directory/ota_md5_zero.bin* /tmp/${version}-pellcorp/$directory/$sub_directory/
+cp /tmp/$old_directory/$old_sub_directory/zero.bin.* /tmp/${version}-pellcorp/$directory/$sub_directory/
+cp /tmp/$old_directory/$old_sub_directory/xImage.* /tmp/${version}-pellcorp/$directory/$sub_directory/
 
 pushd /tmp/${version}-pellcorp/$directory/$sub_directory > /dev/null
 split -d -b 1048576 -a 4 /tmp/${version}-pellcorp/rootfs.squashfs rootfs.squashfs.
@@ -84,11 +98,11 @@ for i in $(ls /tmp/${version}-pellcorp/$directory/$sub_directory/rootfs.squashfs
     echo "$part_md5" >> "/tmp/${version}-pellcorp/$directory/$sub_directory/ota_md5_rootfs.squashfs.${rootfs_md5}"
 done
 
+sed -i "s/ota_version=$old_version/ota_version=$version/g" /tmp/${version}-pellcorp/$directory/$sub_directory/ota_update.in
 sed -i "s/img_md5=$orig_rootfs_md5/img_md5=$rootfs_md5/g" /tmp/${version}-pellcorp/$directory/$sub_directory/ota_update.in
 sed -i "s/img_size=$orig_rootfs_size/img_size=$rootfs_size/g" /tmp/${version}-pellcorp/$directory/$sub_directory/ota_update.in
 
 pushd /tmp/${version}-pellcorp/ > /dev/null
-base_filename=$(basename $filename .img)
-7z a $base_filename.7z -p"$password" $directory
-mv $base_filename.7z $base_filename.img
+7z a ${image_name}.7z -p"$password" $directory
+mv ${image_name}.7z ${image_name}
 popd > /dev/null
