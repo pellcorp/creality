87d7e48a7bb06b6b189526e4241c9dcc0f1d9fcc
Jason Pell <jason@pellcorp.com>
2024-06-20 10:42:30 +1000

    make --header available

--- k1/mcu_file.py
+++ k1/mcu_file.py
@@ -37,6 +37,7 @@ def get_file_metadata(args):
                     length = f"0x{hexlify(reversed_data).decode('utf-8')}"
                     return {
                         'version': version,
+                        'header': version_header,
                         'type': type,
                         'crc16': crc16,
                         'length': length
@@ -49,6 +50,7 @@ def get_file_metadata(args):
 parser = argparse.ArgumentParser(description='Creality K1 MCU Firmware File Metadata')
 parser.add_argument('-f', '--file', type=str, help='firmware file')
 parser.add_argument('-v', '--version', action='store_true', help='Get Version')
+parser.add_argument('-x', '--header', action='store_true', help='Get Version Header')
 parser.add_argument('-t', '--type', action='store_true', help='Get MCU Type')
 parser.add_argument('-c', '--crc16', action='store_true', help='Get CRC16')
 parser.add_argument('-l', '--length', action='store_true', help='Get Length')
@@ -57,6 +59,8 @@ args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
 if args.file:
     details = get_file_metadata(args)
     if details:
+        if args.header:
+            print(details['header'])
         if args.version:
             print(details['version'])
         if args.type:
