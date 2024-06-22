c498f7a464a0aab438d8a56120a1c9b8c46277d1
Jason Pell <jason@pellcorp.com>
2024-06-22 17:26:51 +1000

    reinstall extras that get wiped

---
+++ k1/fix-extras.sh
@@ -0,0 +1,10 @@
+#!/bin/ash
+
+for file in gcode_shell_command.py guppy_config_helper.py calibrate_shaper_config.py guppy_module_loader.py tmcstatus.py; do
+    ln -sf /usr/data/guppyscreen/k1_mods/$file /usr/data/klipper/klippy/extras/$file || exit $?
+    if ! grep -q "klippy/extras/${file}" "/usr/data/klipper/.git/info/exclude"; then
+        echo "klippy/extras/$file" >> "/usr/data/klipper/.git/info/exclude"
+    fi
+done
+/usr/share/klippy-env/bin/python3 -m compileall /usr/data/klipper/klippy
+
