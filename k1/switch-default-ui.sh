b72aff608ed2c327d2f60640cc7cd8a83aeab4ad
Jason Pell <jason@pellcorp.com>
2024-07-01 14:42:08 +1000

    make it possible to switch which ui (fluidd or mainsail) is on port 80

---
+++ k1/switch-default-ui.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ "$1" != "mainsail" ] && [ "$1" != "fluidd" ]; then
+    echo "Invalid choice - must specify either mainsail or fluidd!"
+    exit 1
+fi
+
+cp /usr/data/pellcorp/k1/nginx/fluidd /usr/data/nginx/nginx/sites/ || exit $?
+cp /usr/data/pellcorp/k1/nginx/mainsail /usr/data/nginx/nginx/sites/ || exit $?
+
+if [ "$1" = "mainsail" ]; then
+    sed -i 's/.*listen 80 default_server;/    #listen 80 default_server;/g' /usr/data/nginx/nginx/sites/fluidd || exit $?
+    sed -i 's/.*#listen 80 default_server;/    listen 80 default_server;/g' /usr/data/nginx/nginx/sites/mainsail || exit $?
+else # else fluidd
+    sed -i 's/.*listen 80 default_server;/    #listen 80 default_server;/g' /usr/data/nginx/nginx/sites/mainsail || exit $?
+    sed -i 's/.*#listen 80 default_server;/    listen 80 default_server;/g' /usr/data/nginx/nginx/sites/fluidd || exit $?
+fi
+
+echo "Restarting nginx ..."
+/etc/init.d/S50nginx_service restart
