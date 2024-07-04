78405e930a6c7a7550940c2ed44d209d05163061
Jason Pell <jason@pellcorp.com>
2024-07-04 13:58:54 +1000

    delete all old files to begin with

--- k1/config-overrides.sh
+++ k1/config-overrides.sh
@@ -34,16 +34,9 @@ override_file() {
         cp  /usr/data/printer_data/config/$file /usr/data/pellcorp-overrides/
         return 0
     fi
-
-    if [ -f $overrides_file ]; then
-      rm $overrides_file
-    fi
     $CONFIG_OVERRIDES --original "$original_file" --updated "$updated_file" --overrides "$overrides_file" || exit $?
 
     if [ "$file" = "printer.cfg" ]; then
-      if [ -f /usr/data/pellcorp-overrides/printer.cfg.save_config ]; then
-        rm /usr/data/pellcorp-overrides/printer.cfg.save_config
-      fi
       saves=false
       while IFS= read -r line; do
           if [ "$line" = "#*# <---------------------- SAVE_CONFIG ---------------------->" ]; then
@@ -60,12 +53,23 @@ override_file() {
 
 mkdir -p /usr/data/pellcorp-overrides
 
+# in case we changed config and no longer need an override file, we should delete all
+# all the config files there.
+rm /usr/data/pellcorp-overrides/*.cfg 2> /dev/null
+rm /usr/data/pellcorp-overrides/*.conf 2> /dev/null
+if [ -f /usr/data/pellcorp-overrides/printer.cfg.save_config ]; then
+  rm /usr/data/pellcorp-overrides/printer.cfg.save_config
+fi
+if [ -f /usr/data/pellcorp-overrides/moonraker.secrets ]; then
+  rm /usr/data/pellcorp-overrides/moonraker.secrets
+fi
+
 # special case for moonraker.secrets
 if [ -f /usr/data/printer_data/moonraker.secrets ] && [ -f /usr/data/pellcorp/k1/moonraker.secrets ]; then
     diff /usr/data/printer_data/moonraker.secrets /usr/data/pellcorp/k1/moonraker.secrets > /dev/null
     if [ $? -ne 0 ]; then
         echo "Backing up /usr/data/printer_data/moonraker.secrets..."
-        cp  /usr/data/printer_data/moonraker.secrets /usr/data/pellcorp-overrides/
+        cp /usr/data/printer_data/moonraker.secrets /usr/data/pellcorp-overrides/
     fi
 fi
 
