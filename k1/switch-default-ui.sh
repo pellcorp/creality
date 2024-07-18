31e831d5d06b319a73c542e36399f7e06443871d
Jason Pell <jason@pellcorp.com>
2024-07-18 21:44:04 +1000

    entware nginx ftw

--- k1/switch-default-ui.sh
+++ k1/switch-default-ui.sh
@@ -5,15 +5,14 @@ if [ "$1" != "mainsail" ] && [ "$1" != "fluidd" ]; then
     exit 1
 fi
 
-cp /usr/data/pellcorp/k1/nginx/fluidd /usr/data/nginx/nginx/sites/ || exit $?
-cp /usr/data/pellcorp/k1/nginx/mainsail /usr/data/nginx/nginx/sites/ || exit $?
+cp /usr/data/pellcorp/k1/nginx/* /opt/etc/nginx/sites/ || exit $?
 
 if [ "$1" = "mainsail" ]; then
-    sed -i 's/.*listen 80 default_server;/    #listen 80 default_server;/g' /usr/data/nginx/nginx/sites/fluidd || exit $?
-    sed -i 's/.*#listen 80 default_server;/    listen 80 default_server;/g' /usr/data/nginx/nginx/sites/mainsail || exit $?
+    sed -i 's/.*listen 80 default_server;/    #listen 80 default_server;/g' /opt/etc/nginx/sites/fluidd || exit $?
+    sed -i 's/.*#listen 80 default_server;/    listen 80 default_server;/g' /opt/etc/nginx/sites/mainsail || exit $?
 else # else fluidd
-    sed -i 's/.*listen 80 default_server;/    #listen 80 default_server;/g' /usr/data/nginx/nginx/sites/mainsail || exit $?
-    sed -i 's/.*#listen 80 default_server;/    listen 80 default_server;/g' /usr/data/nginx/nginx/sites/fluidd || exit $?
+    sed -i 's/.*listen 80 default_server;/    #listen 80 default_server;/g' /opt/etc/nginx/sites/mainsail || exit $?
+    sed -i 's/.*#listen 80 default_server;/    listen 80 default_server;/g' /opt/etc/nginx/sites/fluidd || exit $?
 fi
 
 echo "Restarting nginx ..."
