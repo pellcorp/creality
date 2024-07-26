23dcc0c5743143c3a03a2e68800ca5d684cec58a
Jason Pell <jason@pellcorp.com>
2024-07-26 12:27:25 +1000

    no need for fix-extras.sh anymore as we don't have any externally managed klipper extras

--- k1/fix-extras.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for file in guppy_config_helper.py calibrate_shaper_config.py guppy_module_loader.py tmcstatus.py; do
-    ln -sf /usr/data/guppyscreen/k1_mods/$file /usr/data/klipper/klippy/extras/$file || exit $?
-    if ! grep -q "klippy/extras/${file}" "/usr/data/klipper/.git/info/exclude"; then
-        echo "klippy/extras/$file" >> "/usr/data/klipper/.git/info/exclude"
-    fi
-done
-/usr/share/klippy-env/bin/python3 -m compileall /usr/data/klipper/klippy
