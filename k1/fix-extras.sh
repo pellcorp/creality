a737d484ebe28c23ed1695ba345b4844fe3702dd
Jason Pell <jason@pellcorp.com>
2024-07-26 10:00:20 +1000

    moving cartographer.py into pellcorp klipper fork to make things a bit easier

--- k1/fix-extras.sh
+++ k1/fix-extras.sh
@@ -6,5 +6,4 @@ for file in guppy_config_helper.py calibrate_shaper_config.py guppy_module_loade
         echo "klippy/extras/$file" >> "/usr/data/klipper/.git/info/exclude"
     fi
 done
-/usr/data/cartographer-klipper/install.sh || exit $?
 /usr/share/klippy-env/bin/python3 -m compileall /usr/data/klipper/klippy
