c27fbd99b1057489667a527442cf73f9f6e06e66
Jason Pell <jason@pellcorp.com>
2024-09-10 08:54:00 +1000

    allow fan_control.cfg limited overrides

--- k1/apply-overrides.sh
+++ k1/apply-overrides.sh
@@ -31,7 +31,7 @@ function apply_overrides() {
             if [ "$file" = "moonraker.secrets" ]; then
                 echo "Restoring /usr/data/printer_data/$file ..."
                 cp $overrides_dir/$file /usr/data/printer_data/
-            elif [ -L /usr/data/printer_data/config/$file ] || [ "$file" = "guppyscreen.cfg" ] || [ "$file" = "fan_control.cfg" ]; then
+            elif [ -L /usr/data/printer_data/config/$file ] || [ "$file" = "guppyscreen.cfg" ]; then
                 echo "Ignoring $file ..."
             elif [ -f "/usr/data/pellcorp-backups/$file" ] || [ -f "/usr/data/pellcorp/k1/$file" ]; then
               if [ -f /usr/data/printer_data/config/$file ]; then
