97c110e1de7311d0dbd9b2fde2ca591cfe7aeddc
Jason Pell <jason@pellcorp.com>
2024-11-26 18:15:32 +1100

    make sure umount usb after mounting for both check usb key but also for factory reset

--- k1/tools/wait-for-usb-key.sh
+++ k1/tools/wait-for-usb-key.sh
@@ -8,10 +8,12 @@ while true; do
     if [ "$mount" = "/tmp/udisk/sda1" ]; then
       echo
       echo "INFO - USB Key was recognised and mounted correctly ($mount)"
+      umount $mount
       break
     else
       echo
       echo "WARNING: USB Key was recognised and mounted correctly but on a different mount point ($mount)"
+      umount $mount
       break
     fi
   fi
