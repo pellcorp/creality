c4aad12d66d7a29ca22b77eb22f86cd213ca5979
Jason Pell <jason@pellcorp.com>
2024-11-26 17:51:37 +1100

    add a CHECK_USB_KEY macro so that users can verify if a factory reset will even work

---
+++ k1/tools/wait-for-usb.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Waiting for USB Key ..."
+while true; do
+  mounted=$(mount | grep /tmp/udisk/sda)
+  if [ $? -eq 0 ]; then
+    mount=$(echo "$mounted" | awk '{print $3}')
+    if [ "$mount" = "/tmp/udisk/sda1" ]; then
+      echo
+      echo "INFO - USB Key was recognised and mounted correctly ($mount)"
+      break
+    else
+      echo
+      echo "WARNING: USB Key was recognised and mounted correctly but on a different mount point ($mount)"
+      break
+    fi
+  fi
+  sleep 0.5s
+done
