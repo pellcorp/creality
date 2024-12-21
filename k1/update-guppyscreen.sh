173cdfa6c8317f18f38ea60cb4176e8b703604c5
Jason Pell <jason@pellcorp.com>
2024-12-21 19:27:19 +1100

    reconfigure guppy macros

--- k1/update-guppyscreen.sh
+++ k1/update-guppyscreen.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-curl -L "https://github.com/pellcorp/guppyscreen/releases/download/nightly/guppyscreen.tar.gz" -o /usr/data/guppyscreen.tar.gz || exit $?
+target=nightly
+if [ -n "$1" ]; then
+  target=$1
+fi
+curl -L "https://github.com/pellcorp/guppyscreen/releases/download/$target/guppyscreen.tar.gz" -o /usr/data/guppyscreen.tar.gz || exit $?
 tar xf /usr/data/guppyscreen.tar.gz -C /usr/data/ || exit $?
 rm /usr/data/guppyscreen.tar.gz
 /etc/init.d/S99guppyscreen restart
