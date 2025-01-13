f5459fefe2966b2abef209ba68d3a19cb197ac44
Jason Pell <jason@pellcorp.com>
2025-01-13 12:18:54 +1100

    recreate moonraker-env from helper script with more updated dependencies

--- k1/scripts/recreate-moonraker-env.sh
+++ k1/scripts/recreate-moonraker-env.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P)"
-
-wget https://github.com/Guilouz/Creality-Helper-Script/raw/main/files/moonraker/moonraker.tar.gz -O /tmp/moonraker.tar.gz
-tar -zxvf /tmp/moonraker.tar.gz -C /tmp moonraker/moonraker-env
-rm /tmp/moonraker.tar.gz
+K1_DIR="$(dirname $CURRENT_DIR)"
 
 # not sure this is necessary but I want to use the same version of python to update the env
 if [ ! /usr/bin/python3.8 ]; then
@@ -13,17 +10,21 @@ if [ ! /usr/bin/python3.8 ]; then
 fi
 /usr/bin/python3.8 -m ensurepip
 
-/usr/bin/python3.8 -m pip install --target /tmp/moonraker/moonraker-env/lib/python3.8/site-packages/ pyserial-asyncio==0.6
-/usr/bin/python3.8 -m pip install --target /tmp/moonraker/moonraker-env/lib/python3.8/site-packages/ apprise==1.7.1 --no-deps --upgrade
+wget https://github.com/Guilouz/Creality-Helper-Script/raw/main/files/moonraker/moonraker.tar.gz -O /tmp/moonraker.tar.gz
+tar -zxvf /tmp/moonraker.tar.gz -C /tmp moonraker/moonraker-env
+rm /tmp/moonraker.tar.gz
 
 # the env was originally for /usr/share/moonraker-env
-sed -i 's:/usr/share/:/usr/data/:g' /tmp/moonraker/moonraker-env/bin/activate
+#!/usr/data/moonraker/moonraker-env/bin/python
+sed -i 's:/usr/data/moonraker/:/usr/data/:g' /tmp/moonraker/moonraker-env/bin/activate
+sed -i 's:/usr/data/moonraker/:/usr/data/:g' /tmp/moonraker/moonraker-env/bin/pip
+sed -i 's:/usr/data/moonraker/:/usr/data/:g' /tmp/moonraker/moonraker-env/bin/pip3
 
 # and we are never going to need these
 rm /tmp/moonraker/moonraker-env/bin/activate.csh
 rm /tmp/moonraker/moonraker-env/bin/activate.fish
 
 pushd /tmp/moonraker > /dev/null
-tar -zcf $CURRENT_DIR/moonraker-env.tar.gz moonraker-env || exit $?
+tar -zcf $K1_DIR/moonraker-env.tar.gz moonraker-env || exit $?
 popd > /dev/null
 rm -rf /tmp/moonraker
