5e000a7c54f1aee474aa845d107bd1bc9df9c4ff
Jason Pell <jason@pellcorp.com>
2025-02-20 10:09:52 +1100

    make sure that existing guppyscreen update for nightly will get updated to main

--- k1/update-guppyscreen.sh
+++ k1/update-guppyscreen.sh
@@ -38,7 +38,7 @@ fi
 
 if [ "$update_guppyscreen" = "true" ]; then
     target=main
-    if [ -n "$1" ]; then
+    if [ -n "$1" ] && [ "$1" != "nightly" ]; then
       target=$1
     fi
     curl -L "https://github.com/pellcorp/guppyscreen/releases/download/$target/guppyscreen.tar.gz" -o /usr/data/guppyscreen.tar.gz || exit $?
