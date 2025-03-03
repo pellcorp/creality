b4fde065e33ebfd38fb5ed9b56bd32710fbc549f
Jason Pell <jason@pellcorp.com>
2025-03-03 11:34:33 +1100

    fix up git-ssh.sh script to handle being used without already being cloned

--- k1/ssh/git-ssh.sh
+++ k1/ssh/git-ssh.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-dbclient -y -i /usr/data/pellcorp/k1/ssh/$GIT_SSH_IDENTITY-identity $* 2> /dev/null
+if [ -f /usr/data/pellcorp/k1/ssh/$GIT_SSH_IDENTITY-identity ]; then
+    dbclient -y -i /usr/data/pellcorp/k1/ssh/$GIT_SSH_IDENTITY-identity $* 2> /dev/null
+elif [ -f /root/.ssh/$GIT_SSH_IDENTITY-identity ]; then
+    dbclient -y -i /root/.ssh/$GIT_SSH_IDENTITY-identity $* 2> /dev/null
+else
+    echo "ERROR: No $GIT_SSH_IDENTITY-identity identity"
+    exit 1
+fi
