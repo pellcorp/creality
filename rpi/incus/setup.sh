dd40a82355a1153bdd378021cd2f07437c55159a
Jason Pell <jason@pellcorp.com>
2025-07-26 08:10:22 +1000

    oops was still trying to update pi user instead of me user

--- rpi/incus/setup.sh
+++ rpi/incus/setup.sh
@@ -6,7 +6,7 @@ systemctl enable ssh 2> /dev/null
 echo "%sudo  ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/nopasswd > /dev/null
 sudo useradd -m -s /bin/bash me
 sudo usermod -a -G sudo me
-echo "pi:raspberry" | sudo chpasswd
+echo "me:raspberry" | sudo chpasswd
 mkdir -p /home/me/.ssh
 cat /root/id_rsa.pub >> /home/me/.ssh/authorized_keys
 sudo chown -R me: /home/me/.ssh
