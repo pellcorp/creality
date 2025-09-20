ae41180600ff15d01c82a31497eebff0c38a0e56
Jason Pell <jason@pellcorp.com>
2025-09-20 12:39:59 +1000

    slight fix for switch to stock for KE and force update of grumpyscreen with wifi and some display fixes for KE (#924)

--- rpi/install-grumpyscreen.sh
+++ rpi/install-grumpyscreen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # this allows us to make changes to Simple AF and grumpyscreen in parallel
-GRUMPYSCREEN_TIMESTAMP=1756264500
+GRUMPYSCREEN_TIMESTAMP=1758335800
 
 BASEDIR=$HOME
 source $BASEDIR/pellcorp/rpi/functions.sh
