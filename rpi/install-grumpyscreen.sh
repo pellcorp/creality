b76a28596bdf0a882530ce0e2c1160a66d8dfafd
Jason Pell <jason@pellcorp.com>
2025-10-23 07:27:40 +1100

    thanks to some investigation by tbk with some assistance from Chad we (#968)
    are going to remove the M106 and M107 custom macros and instead extend
    klipper to support fan_generic for aux and chamber but use a normal fan
    for part.  The change has not been applied to Simple AF for RPi as yet.

--- rpi/install-grumpyscreen.sh
+++ rpi/install-grumpyscreen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # this allows us to make changes to Simple AF and grumpyscreen in parallel
-GRUMPYSCREEN_TIMESTAMP=1758872100
+GRUMPYSCREEN_TIMESTAMP=1761125800
 
 BASEDIR=$HOME
 source $BASEDIR/pellcorp/rpi/functions.sh
