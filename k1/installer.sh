23f365506cc2ec743acc6e7963af0b3e84e97512
Jason Pell <jason@pellcorp.com>
2025-10-24 21:28:22 +1100

    Revert "no idea why we are starting bsa_server on the nebula pad but its using a lot of ram (#971)"
    This reverts commit d5b535e0c4c02bd6874a051635e6c8a62b2e4398.

--- k1/installer.sh
+++ k1/installer.sh
@@ -221,11 +221,6 @@ function disable_creality_services() {
       mount -o remount /
     fi
 
-    # the bsa_server uses like 35% of ram its a bitch we dont need it
-    if [ -f /etc/init.d/S41bt_bsa_download_firmware ]; then
-        rm /etc/init.d/S41bt_bsa_download_firmware
-    fi
-    
     if [ -f /etc/init.d/S99start_app ]; then
         echo
         echo "INFO: Disabling some creality services ..."
