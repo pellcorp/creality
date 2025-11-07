0a9bf25fa432da0ae91f406045c12c8168a61c3f
Jason Pell <jason@pellcorp.com>
2025-11-07 15:25:36 +1100

    as part of an effort to migrate the existing guppyscreen.json to an ini file, extracting the calibration data for KE and RPi to calibration.json file

--- k1/installer.sh
+++ k1/installer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # this allows us to make changes to Simple AF and grumpyscreen in parallel
-GRUMPYSCREEN_TIMESTAMP=1761456100
+GRUMPYSCREEN_TIMESTAMP=1762489200
 
 if [ -f /usr/bin/get_sn_mac.sh ]; then
   MODEL=$(/usr/bin/get_sn_mac.sh model)
--- rpi/install-grumpyscreen.sh
+++ rpi/install-grumpyscreen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # this allows us to make changes to Simple AF and grumpyscreen in parallel
-GRUMPYSCREEN_TIMESTAMP=1761456100
+GRUMPYSCREEN_TIMESTAMP=1762489200
 
 BASEDIR=$HOME
 source $BASEDIR/pellcorp/rpi/functions.sh
