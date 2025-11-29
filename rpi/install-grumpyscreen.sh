69736da165119df0ee342d67be324ad21ec1ac27
Jason Pell <jason@pellcorp.com>
2025-11-29 13:52:02 +1100

    pass wpa supplicant socket in via env variable not config

--- rpi/install-grumpyscreen.sh
+++ rpi/install-grumpyscreen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # this allows us to make changes to Simple AF and grumpyscreen in parallel
-GRUMPYSCREEN_TIMESTAMP=1762663700
+GRUMPYSCREEN_TIMESTAMP=1764387900
 
 BASEDIR=$HOME
 source $BASEDIR/pellcorp/rpi/functions.sh
