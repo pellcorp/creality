63a424cb3e2d009c55af86516012e1b3ce140acf
Jason Pell <jason@pellcorp.com>
2025-12-04 15:08:07 +1100

    force an update to latest grumpyscreen with new fixes including separate sys info tab, support log in tools menu, tools instead of settings (#1029)

--- k1/installer.sh
+++ k1/installer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # this allows us to make changes to Simple AF and grumpyscreen in parallel
-GRUMPYSCREEN_TIMESTAMP=1764637200
+GRUMPYSCREEN_TIMESTAMP=1764819600
 GRUMPYSCREEN_BRANCH=main
 
 if [ -f /usr/bin/get_sn_mac.sh ]; then
