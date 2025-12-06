cdaab5a0c9519c0d45e29d156f67d26146f1934f
Jason Pell <jason@pellcorp.com>
2025-12-07 09:21:35 +1100

    lets get everyone on latest grumpy (#1046)

--- rpi/install-grumpyscreen.sh
+++ rpi/install-grumpyscreen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # this allows us to make changes to Simple AF and grumpyscreen in parallel
-GRUMPYSCREEN_TIMESTAMP=1764998000
+GRUMPYSCREEN_TIMESTAMP=1765004300
 
 BASEDIR=$HOME
 source $BASEDIR/pellcorp/rpi/functions.sh
