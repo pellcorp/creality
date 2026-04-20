d510f7a953218aef4025efb0a1d1277aec4ef878
Jason Pell <jason@pellcorp.com>
2026-04-20 12:59:22 +1000

    v5 crowsnest installation (#1273)

--- rpi/install-crowsnest.sh
+++ rpi/install-crowsnest.sh
@@ -26,26 +26,12 @@ if [ $? -ne 0 ]; then
       retry sudo DEBIAN_FRONTEND=noninteractive apt-get --yes install make; error
     fi
 
-    echo
-    echo "INFO: Building crowsnest quietly ... (this is potentially going to take a long time)"
-    echo
-    echo "If you wish to monitor progress of building crowsnest, open another terminal and run:"
-    echo "  tail -f $BASEDIR/printer_data/logs/crowsnest-install-$TIMESTAMP.log"
-    echo
-    
-    sudo CROWSNEST_UNATTENDED=1 CROWSNEST_ADD_CROWSNEST_MOONRAKER=0 make install > $BASEDIR/printer_data/logs/crowsnest-install-$TIMESTAMP.log 2>&1
-    if [ $? -eq 0 ]; then
-      echo "INFO: Crownest Installation complete!"
-    else
-      echo "ERROR: Crowsnest installation failed - see $BASEDIR/printer_data/logs/crowsnest-install-$TIMESTAMP.log for error"
-      exit 1
-    fi
+    sudo CROWSNEST_UNATTENDED=1 CROWSNEST_ADD_CROWSNEST_MOONRAKER=1 make install || exit $?
 
     # we replace the one copied in there with ours so that config overrides work
     cp $BASEDIR/pellcorp/rpi/crowsnest.conf $BASEDIR/printer_data/config/ || exit $?
   fi
 
-  $CONFIG_HELPER --file moonraker.conf --overrides $BASEDIR/pellcorp/rpi/crowsnest-um.conf --quiet || exit $?
   cp $BASEDIR/pellcorp/rpi/webcam.conf $BASEDIR/printer_data/config/ || exit $?
   $CONFIG_HELPER --file moonraker.conf --add-include "webcam.conf" || exit $?
   sudo systemctl restart crowsnest
