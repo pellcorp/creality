68dea35605116fc9c1045db830ed75ae1059782f
Jason Pell <jason@pellcorp.com>
2026-05-19 07:06:21 +1000

    some tweaks to try and resolve numpad issues with grumpyscreen have been released force an upgrade

--- k1/installer.sh
+++ k1/installer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # this allows us to make changes to Simple AF and grumpyscreen in parallel
-GRUMPYSCREEN_TIMESTAMP=1779070000
+GRUMPYSCREEN_TIMESTAMP=1779138100
 GRUMPYSCREEN_BRANCH=main
 
 if [ -f /usr/bin/get_sn_mac.sh ]; then
