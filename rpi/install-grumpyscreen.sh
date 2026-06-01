b5a48a59f244eda7814edf381129526ec2708b98
Jason Pell <jason@pellcorp.com>
2026-06-01 15:28:09 +1000

    add grumpyscreen extruder panel config

--- rpi/install-grumpyscreen.sh
+++ rpi/install-grumpyscreen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # this allows us to make changes to Simple AF and grumpyscreen in parallel
-GRUMPYSCREEN_TIMESTAMP=1779138100
+GRUMPYSCREEN_TIMESTAMP=1780291500
 
 BASEDIR=$HOME
 source $BASEDIR/pellcorp/rpi/functions.sh
