7c8a53d6af8a88fd7953dc4d755a55cd1d07e1d5
Jason Pell <jason@pellcorp.com>
2026-07-26 12:09:20 +1000

    custom boot display for k1, k1m, etc and Nebula pad and Ender 5 Max to avoid completely stuffing up other machines

--- k1/installer.sh
+++ k1/installer.sh
@@ -426,18 +426,21 @@ function install_boot_display() {
     echo "INFO: Installing custom boot display ..."
 
     # shamelessly stolen from https://github.com/Guilouz/Creality-Helper-Script/blob/main/scripts/custom_boot_display.sh
-    rm -rf /etc/boot-display/*
     cp /usr/data/pellcorp/k1/services/S11jpeg_display_shell /etc/init.d/
     mkdir -p /usr/data/boot-display
 
     if [ "$model" = "k1" ] || [ "$model" = "k1m" ]; then
+      rm -rf /etc/boot-display/*
       cp /usr/data/pellcorp/k1/boot-display.conf /etc/boot-display/
       tar -zxf /usr/data/pellcorp/k1/boot-display.tar.gz -C /usr/data/boot-display || exit $?
-    else
+      ln -s /usr/data/boot-display/part0 /etc/boot-display/
+    elif [ "$MODEL" = "F005" ]; then # default boot display for Ender 3 V3 and Ender 5 Max for now
+      rm -rf /etc/boot-display/*
       cp /usr/data/pellcorp/k1/boot-display-nebula.conf /etc/boot-display/boot-display.conf
       tar -zxf /usr/data/pellcorp/k1/boot-display-nebula.tar.gz -C /usr/data/boot-display || exit $?
+      ln -s /usr/data/boot-display/part0 /etc/boot-display/
     fi
-    ln -s /usr/data/boot-display/part0 /etc/boot-display/
+
     echo "boot-display" >> /usr/data/pellcorp.done
     sync
   fi
